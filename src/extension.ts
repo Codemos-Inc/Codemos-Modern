aa0c43e38a7a105155d77b46ffc6f7b9dfa6b757
EmrecanKaracayir <dev.emrecan.karacayir@gmail.com>
2023-06-25 16:25:05 +0300

    Add: State handling via globalState

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,21 @@ import * as theme from "./theme";
 import { checkHex, contrastChecker } from "./util/color";
 
 export enum ColorMode {
-  dark = 0,
-  light = 1,
+  dark,
+  light,
 }
 
 export enum AdaptiveMode {
-  none = 0,
-  gentle = 15,
-  aggressive = 25,
+  none,
+  gentle,
+  aggressive,
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  retrieveGlobalState(context);
   context.subscriptions.push(
     vscode.commands.registerCommand("codemos-modern.theme", async () => {
+      // Color mode
       const colorModeStr = await vscode.window.showQuickPick(
         [
           { label: "Dark", description: "Color Mode" },
@@ -27,20 +29,11 @@ export function activate(context: vscode.ExtensionContext) {
           ignoreFocusOut: true,
         }
       );
-      let colorMode: ColorMode;
-      if (colorModeStr === undefined) {
+      if (!colorModeStr) {
         return;
       }
-      switch (colorModeStr.label) {
-        case "Dark":
-          colorMode = ColorMode.dark;
-          break;
-        case "Light":
-          colorMode = ColorMode.light;
-          break;
-        default:
-          return;
-      }
+      const colorMode = colorModeInputHelper(colorModeStr.label);
+      // Accent color
       const accentHex7 = await vscode.window.showInputBox({
         title: `Codemos Modern ${colorModeStr.label} 2/3`,
         prompt: "Accent color in hex color code (e.g., #60CDFF)",
@@ -48,34 +41,14 @@ export function activate(context: vscode.ExtensionContext) {
         valueSelection: [1, 7],
         ignoreFocusOut: true,
       });
-      if (accentHex7 === undefined) {
+      if (!accentHex7) {
         return;
       }
-      const accentHex = accentHex7.substring(1);
-      if (!checkHex(accentHex)) {
-        await vscode.window.showErrorMessage("Invalid color code");
+      const accentHex = await accentColorInputHelper(accentHex7, colorMode);
+      if (!accentHex) {
         return;
       }
-      switch (colorMode) {
-        case ColorMode.dark:
-          if (!contrastChecker(accentHex, "000000")) {
-            await vscode.window.showErrorMessage(
-              "Accent color must have at least 4.5:1 contrast with black"
-            );
-            return;
-          }
-          break;
-        case ColorMode.light:
-          if (!contrastChecker(accentHex, "FFFFFF")) {
-            await vscode.window.showErrorMessage(
-              "Accent color must have at least 4.5:1 contrast with white"
-            );
-            return;
-          }
-          break;
-        default:
-          return;
-      }
+      // Adaptive mode
       const adaptiveModeStr = await vscode.window.showQuickPick(
         [
           { label: "None", description: "Adaptive Mode" },
@@ -89,56 +62,215 @@ export function activate(context: vscode.ExtensionContext) {
           ignoreFocusOut: true,
         }
       );
-      let adaptiveMode: AdaptiveMode;
-      if (adaptiveModeStr === undefined) {
+      if (!adaptiveModeStr) {
         return;
       }
-      switch (adaptiveModeStr.label) {
-        case "None":
-          adaptiveMode = AdaptiveMode.none;
-          break;
-        case "Gentle":
-          adaptiveMode = AdaptiveMode.gentle;
-          break;
-        case "Aggressive":
-          adaptiveMode = AdaptiveMode.aggressive;
-          break;
-        default:
-          return;
-      }
-      await theme.createTheme(accentHex, colorMode, adaptiveMode);
-      switch (colorMode) {
-        case ColorMode.dark:
-          await vscode.workspace
-            .getConfiguration()
-            .update(
-              "workbench.colorTheme",
-              "Codemos Modern (Dark)",
-              vscode.ConfigurationTarget.Global
-            );
-          break;
-        case ColorMode.light:
-          await vscode.workspace
-            .getConfiguration()
-            .update(
-              "workbench.colorTheme",
-              "Codemos Modern (Light)",
-              vscode.ConfigurationTarget.Global
-            );
-          break;
-        default:
-          return;
-      }
+      const adaptiveMode = adaptiveModeInputHelper(adaptiveModeStr.label);
+      // Make theme
+      await makeTheme(context, accentHex, colorMode, adaptiveMode);
+      // Apply theme
+      await applyTheme(colorMode);
+      // Inform User && Reload window
       await vscode.window
         .showInformationMessage(
           "Your Codemos Modern is ready!",
-          "Apply Codemos Modern"
+          "Apply",
+          "Later"
         )
         .then((selectedAction) => {
-          if (selectedAction === "Apply Codemos Modern") {
+          if (selectedAction === "Apply") {
             vscode.commands.executeCommand("workbench.action.reloadWindow");
           }
         });
     })
   );
+}
+
+async function makeTheme(
+  context: vscode.ExtensionContext,
+  accentHex: string,
+  colorMode: ColorMode,
+  adaptiveMode: AdaptiveMode
+) {
+  // Create theme
+  await theme.createTheme(accentHex, colorMode, adaptiveMode);
+  // Save global state
+  await saveGlobalState(context, accentHex, colorMode, adaptiveMode);
+}
+
+async function applyTheme(colorMode: ColorMode) {
+  switch (colorMode) {
+    case ColorMode.dark:
+      await vscode.workspace
+        .getConfiguration()
+        .update(
+          "workbench.colorTheme",
+          "Codemos Modern (Dark)",
+          vscode.ConfigurationTarget.Global
+        );
+      break;
+    case ColorMode.light:
+      await vscode.workspace
+        .getConfiguration()
+        .update(
+          "workbench.colorTheme",
+          "Codemos Modern (Light)",
+          vscode.ConfigurationTarget.Global
+        );
+      break;
+    default:
+      return;
+  }
+}
+
+async function retrieveGlobalState(context: vscode.ExtensionContext) {
+  // Retrieve saved version
+  const savedVersion: undefined | string = context.globalState.get(
+    "codemos-modern.version"
+  );
+  if (!savedVersion || savedVersion === context.extension.packageJSON.version) {
+    return;
+  }
+  // Retrieve saved color mode
+  const savedColorMode: undefined | ColorMode = context.globalState.get(
+    "codemos-modern.colorMode"
+  );
+  if (!savedColorMode) {
+    return;
+  }
+  // Retrieve saved dark variant
+  const darkAccentHex: undefined | string = context.globalState.get(
+    "codemos-modern.dark.accentHex"
+  );
+  const darkAdaptiveMode: undefined | AdaptiveMode = context.globalState.get(
+    "codemos-modern.dark.adaptiveMode"
+  );
+  if (darkAccentHex && darkAdaptiveMode) {
+    await makeTheme(context, darkAccentHex, ColorMode.dark, darkAdaptiveMode);
+  }
+  // Retrieve saved light variant
+  const lightAccentHex: undefined | string = context.globalState.get(
+    "codemos-modern.light.accentHex"
+  );
+  const lightAdaptiveMode: undefined | AdaptiveMode = context.globalState.get(
+    "codemos-modern.light.adaptiveMode"
+  );
+  if (lightAccentHex && lightAdaptiveMode) {
+    await makeTheme(
+      context,
+      lightAccentHex,
+      ColorMode.light,
+      lightAdaptiveMode
+    );
+  }
+  // Inform User && Reload window
+  await vscode.window
+    .showInformationMessage(
+      "Your Codemos Modern has been updated!",
+      "Apply",
+      "Later"
+    )
+    .then((selectedAction) => {
+      if (selectedAction === "Apply") {
+        applyTheme(savedColorMode);
+        vscode.commands.executeCommand("workbench.action.reloadWindow");
+      }
+    });
+}
+
+async function saveGlobalState(
+  context: vscode.ExtensionContext,
+  accentHex: string,
+  colorMode: ColorMode,
+  adaptiveMode: AdaptiveMode
+) {
+  await context.globalState.update(
+    "codemos-modern.version",
+    context.extension.packageJSON.version
+  );
+  await context.globalState.update("codemos-modern.colorMode", colorMode);
+  switch (colorMode) {
+    case ColorMode.dark:
+      await context.globalState.update(
+        "codemos-modern.dark.accentHex",
+        accentHex
+      );
+      await context.globalState.update(
+        "codemos-modern.dark.adaptiveMode",
+        adaptiveMode
+      );
+      break;
+    case ColorMode.light:
+      await context.globalState.update(
+        "codemos-modern.light.accentHex",
+        accentHex
+      );
+      await context.globalState.update(
+        "codemos-modern.light.adaptiveMode",
+        adaptiveMode
+      );
+      break;
+    default:
+      return;
+  }
+}
+
+function colorModeInputHelper(label: string): ColorMode {
+  switch (label) {
+    case "Dark":
+      return ColorMode.dark;
+    case "Light":
+      return ColorMode.light;
+    default:
+      return ColorMode.dark;
+  }
+}
+
+async function accentColorInputHelper(
+  accentHex7: string,
+  colorMode: ColorMode
+) {
+  const accentHex = accentHex7.substring(1);
+  if (!checkHex(accentHex)) {
+    await vscode.window.showErrorMessage("Invalid color code");
+    return undefined;
+  }
+  switch (colorMode) {
+    case ColorMode.dark:
+      if (!contrastChecker(accentHex, "000000")) {
+        await vscode.window.showErrorMessage(
+          "Accent color must have at least 4.5:1 contrast with black"
+        );
+        return undefined;
+      }
+      break;
+    case ColorMode.light:
+      if (!contrastChecker(accentHex, "FFFFFF")) {
+        await vscode.window.showErrorMessage(
+          "Accent color must have at least 4.5:1 contrast with white"
+        );
+        return undefined;
+      }
+      break;
+    default:
+      return undefined;
+  }
+  return accentHex;
+}
+
+function adaptiveModeInputHelper(label: string): AdaptiveMode {
+  switch (label) {
+    case "None":
+      return AdaptiveMode.none;
+    case "Gentle":
+      return AdaptiveMode.gentle;
+    case "Aggressive":
+      return AdaptiveMode.aggressive;
+    default:
+      return AdaptiveMode.none;
+  }
+}
+
+export function deactivate() {
+  return;
 }
