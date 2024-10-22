0c4a20cced036d93b18b24c9ff7f28dcbe188333
EmrecanKaracayir <developer@karacayir.com>
2024-10-23 02:18:29 +0300

    Fixed: 0 accounted as false.

--- src/extension/commands/configure/index.ts
+++ src/extension/commands/configure/index.ts
@@ -18,13 +18,13 @@ export const configureCommand = async () => {
   // >----------< Variant selection >----------<
   const variant = await variantView();
   // Dismissed, exit command
-  if (!variant) {
+  if (variant === null) {
     return;
   }
   // >----------< UI aux extension selection >----------<
   const auxUiExtensionId = await extensionView(variant, "ui", isOnlineAvailable);
   // Dismissed, exit command
-  if (!auxUiExtensionId) {
+  if (auxUiExtensionId === null) {
     return;
   }
   let auxUiThemeId = null;
@@ -32,21 +32,21 @@ export const configureCommand = async () => {
     // >----------< UI aux theme selection >----------<
     const auxUiThemeCandidate = await themeView(variant, auxUiExtensionId, "ui", isOnlineAvailable);
     // Dismissed, exit command
-    if (!auxUiThemeCandidate) {
+    if (auxUiThemeCandidate === null) {
       return;
     }
     auxUiThemeId = auxUiThemeCandidate;
   }
   // >----------< Design selection (IF NOT AUX) >----------<
-  const design = !auxUiThemeId ? await designView(variant) : null;
+  const design = auxUiThemeId === null ? await designView(variant) : null;
   // Dismissed, exit command
-  if (!design && !auxUiThemeId) {
+  if (design === null && auxUiThemeId === null) {
     return;
   }
   // >----------< Accent color selection (IF NOT AUX) >----------<
-  const accentColorSelect = !auxUiThemeId ? await accentView(variant) : null;
+  const accentColorSelect = auxUiThemeId === null ? await accentView(variant) : null;
   // Dismissed, exit command
-  if (!accentColorSelect && !auxUiThemeId) {
+  if (accentColorSelect === null && auxUiThemeId === null) {
     return;
   }
   let accentColor: string | null;
@@ -54,16 +54,17 @@ export const configureCommand = async () => {
     accentColor = accentColorSelect;
   } else {
     // >----------< Custom accent color selection (IF NOT AUX) >----------<
-    accentColor = !auxUiThemeId ? await accentInputView(variant) : null;
+    accentColor = auxUiThemeId === null ? await accentInputView(variant) : null;
     // Dismissed, exit command
-    if (!accentColor && !auxUiThemeId) {
+    if (accentColor === null && auxUiThemeId === null) {
       return;
     }
   }
   // >----------< Adaptation color selection (IF NOT AUX) >----------<
-  const adaptationColorSelect = !auxUiThemeId ? await adaptationView(variant, accentColor!) : null;
+  const adaptationColorSelect =
+    auxUiThemeId === null ? await adaptationView(variant, accentColor!) : null;
   // Dismissed, exit command
-  if (!adaptationColorSelect && !auxUiThemeId) {
+  if (adaptationColorSelect === null && auxUiThemeId === null) {
     return;
   }
   let adaptationColor: string | null;
@@ -76,16 +77,17 @@ export const configureCommand = async () => {
     }
   } else {
     // >----------< Custom adaptation color selection (IF NOT AUX) >----------<
-    adaptationColor = !auxUiThemeId ? await adaptationInputView(variant) : null;
+    adaptationColor = auxUiThemeId === null ? await adaptationInputView(variant) : null;
     // Dismissed, exit command
-    if (!adaptationColor && !auxUiThemeId) {
+    if (adaptationColor === null && auxUiThemeId === null) {
       return;
     }
   }
   // >----------< Adaptation intensity selection (IF NOT AUX) >----------<
-  const intensitySelect = !auxUiThemeId ? await intensityView(variant, adaptationColor!) : null;
+  const intensitySelect =
+    auxUiThemeId === null ? await intensityView(variant, adaptationColor!) : null;
   // Dismissed, exit command
-  if (!intensitySelect && !auxUiThemeId) {
+  if (intensitySelect === null && auxUiThemeId === null) {
     return;
   }
   let intensityNumber: number | null;
@@ -93,16 +95,16 @@ export const configureCommand = async () => {
     intensityNumber = intensitySelect;
   } else {
     // >----------< Custom adaptation intensity selection (IF NOT AUX) >----------<
-    intensityNumber = !auxUiThemeId ? await intensityInputView(variant) : null;
+    intensityNumber = auxUiThemeId === null ? await intensityInputView(variant) : null;
     // Dismissed, exit command
-    if (!intensityNumber && !auxUiThemeId) {
+    if (intensityNumber === null && auxUiThemeId === null) {
       return;
     }
   }
   // >----------< Code aux extension selection >----------<
   const auxCodeThemeExtId = await extensionView(variant, "code", isOnlineAvailable);
   // Dismissed, exit command
-  if (!auxCodeThemeExtId) {
+  if (auxCodeThemeExtId === null) {
     return;
   }
   let auxCodeThemeId = null;
@@ -115,7 +117,7 @@ export const configureCommand = async () => {
       isOnlineAvailable,
     );
     // Dismissed, exit command
-    if (!auxCodeThemeCandidate) {
+    if (auxCodeThemeCandidate === null) {
       return;
     }
     auxCodeThemeId = auxCodeThemeCandidate;
@@ -125,10 +127,10 @@ export const configureCommand = async () => {
   await updateConfig(
     variant,
     auxUiThemeId,
-    !design ? getConfig()[variant].design : design,
-    !accentColor ? getConfig()[variant].accentColor : accentColor,
-    !adaptationColor ? getConfig()[variant].adaptationColor : adaptationColor,
-    !intensityNumber ? getConfig()[variant].adaptationIntensity : intensityNumber,
+    design === null ? getConfig()[variant].design : design,
+    accentColor === null ? getConfig()[variant].accentColor : accentColor,
+    adaptationColor === null ? getConfig()[variant].adaptationColor : adaptationColor,
+    intensityNumber === null ? getConfig()[variant].adaptationIntensity : intensityNumber,
     auxCodeThemeId,
   );
   await applyTheme(variant);
